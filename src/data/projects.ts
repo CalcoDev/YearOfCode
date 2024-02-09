0574f69984a64f5a3124b18cd9f0a149195b2b76
Calco Dev <calcopoddev@gmail.com>
2024-02-09 14:38:55 +0200

    Moved projects. Accesibility changes.

---
+++ src/data/projects.ts
@@ -0,0 +1,28 @@
+const projects = [
+    {
+        title: "year of code",
+        description: "Literally this website",
+    },
+    {
+        title: "cave",
+        description: "Funky atmospheric game that I lost motivation for.",
+    },
+    {
+        title: "untitled website",
+        description: "The homepage of the Obeya",
+    },
+    {
+        title: "my mother told me",
+        description: "Someday I will buy. Galley with good oars..."
+    },
+    {
+        title: "Pixcandescent",
+        description: "So so so many pixels!"
+    },
+    {
+        title: "Ultrakongle",
+        description: "Ultrakill but more ... kongle. Also has multiplayer."
+    }
+];
+
+export default projects;
