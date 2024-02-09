97e460802098cfebb97865658db6748f17a3b552
Calco Dev <calcopoddev@gmail.com>
2024-02-09 15:12:22 +0200

    Projects component and dynamic page refactor.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,27 +2,33 @@ const projects = [
     {
         title: "year of code",
         description: "Literally this website",
+        github: "https://github.com/CalcoDev",
     },
     {
         title: "cave",
         description: "Funky atmospheric game that I lost motivation for.",
+        github: "https://github.com/CalcoDev",
     },
     {
         title: "untitled website",
         description: "The homepage of the Obeya",
+        github: "https://github.com/CalcoDev",
     },
     {
         title: "my mother told me",
-        description: "Someday I will buy. Galley with good oars..."
+        description: "Someday I will buy. Galley with good oars...",
+        github: "https://github.com/CalcoDev",
     },
     {
         title: "Pixcandescent",
-        description: "So so so many pixels!"
+        description: "So so so many pixels!",
+        github: "https://github.com/CalcoDev",
     },
     {
         title: "Ultrakongle",
-        description: "Ultrakill but more ... kongle. Also has multiplayer."
-    }
+        description: "Ultrakill but more ... kongle. Also has multiplayer.",
+        github: "https://github.com/CalcoDev",
+    },
 ];
 
 export default projects;
